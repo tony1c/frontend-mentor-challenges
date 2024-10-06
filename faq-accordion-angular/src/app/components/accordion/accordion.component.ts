6e8b16269f5ef405d3cfc505c112f1adc36c3590
tony1c <antoniomario.cavuto@gmail.com>
2024-10-06 16:56:29 +0200

    accordion update

--- faq-accordion-angular/src/app/components/accordion/accordion.component.ts
+++ faq-accordion-angular/src/app/components/accordion/accordion.component.ts
@@ -1,5 +1,4 @@
-import { Component, inject, Input } from '@angular/core';
-import { AccordionService } from '../../services/accordion.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-accordion',
@@ -9,15 +8,40 @@ import { AccordionService } from '../../services/accordion.service';
   styleUrl: './accordion.component.css',
 })
 export class AccordionComponent {
-  @Input() question = '';
-  @Input() response = '';
-  accordionService: AccordionService = inject(AccordionService);
+  accordions = [
+    {
+      id: 0,
+      question: 'What is Frontend Mentor, and how will it help me?',
+      response:
+        "Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building.",
+    },
+    {
+      id: 1,
+      question: 'Is Frontend Mentor free?',
+      response:
+        'Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels.',
+    },
+    {
+      id: 2,
+      question: 'Can I use Frontend Mentor projects in my portfolio?',
+      response:
+        "Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent way to showcase your skills to potential employers!",
+    },
+    {
+      id: 3,
+      question:
+        "How can I get help if I'm stuck on a Frontend Mentor challenge?",
+      response:
+        "The best place to get help is inside Frontend Mentor's Discord community. There's a help channel where you can ask questions and seek support from other community members.",
+    },
+  ];
+  expandedIdx: number | null = null;
 
-  public log() {
-    console.log('Clicked');
-  }
-
-  public toggleIsOpen() {
-    this.accordionService.toggleIsOpen();
+  public toggleAccordion(id: number): void {
+    if (this.expandedIdx === id) {
+      this.expandedIdx = null;
+    } else {
+      this.expandedIdx = id;
+    }
   }
 }

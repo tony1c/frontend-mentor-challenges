7e6a0166057cc6c57c4f4c142e43a91c64a82f3a
tony1c <antoniomario.cavuto@gmail.com>
2024-10-06 13:37:04 +0200

    first commit accordion frontend mentor

---
+++ faq-accordion-angular/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    colors: {
+      white: "hsl(0, 0%, 100%)",
+      "light-pink": "#f9f0ff",
+      "grayish-purple": "hsl(292, 16%, 49%)",
+      "dark-purple": "hsl(292, 42%, 14%)",
+    },
+    fontWeight: {
+      400: "400",
+      600: "600",
+      700: "700",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Work Sans", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};

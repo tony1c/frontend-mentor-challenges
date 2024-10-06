950d38d17a13ee6c1fc572a15ceea54de10b491e
tony1c <antoniomario.cavuto@gmail.com>
2024-10-06 17:02:50 +0200

    accordion service + model

--- faq-accordion-angular/src/app/components/accordion/accordion.component.ts
+++ faq-accordion-angular/src/app/components/accordion/accordion.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AccordionService } from '../../services/accordion.service';
+import { AccordionModel } from '../../models/accordion.model';
 
 @Component({
   selector: 'app-accordion',
@@ -8,33 +10,8 @@ import { Component } from '@angular/core';
   styleUrl: './accordion.component.css',
 })
 export class AccordionComponent {
-  accordions = [
-    {
-      id: 0,
-      question: 'What is Frontend Mentor, and how will it help me?',
-      response:
-        "Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building.",
-    },
-    {
-      id: 1,
-      question: 'Is Frontend Mentor free?',
-      response:
-        'Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels.',
-    },
-    {
-      id: 2,
-      question: 'Can I use Frontend Mentor projects in my portfolio?',
-      response:
-        "Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent way to showcase your skills to potential employers!",
-    },
-    {
-      id: 3,
-      question:
-        "How can I get help if I'm stuck on a Frontend Mentor challenge?",
-      response:
-        "The best place to get help is inside Frontend Mentor's Discord community. There's a help channel where you can ask questions and seek support from other community members.",
-    },
-  ];
+  accordionService: AccordionService = inject(AccordionService);
+  accordions: AccordionModel[] = this.accordionService.getAccordions();
   expandedIdx: number | null = null;
 
   public toggleAccordion(id: number): void {
---
+++ faq-accordion-angular/src/app/services/accordion.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { AccordionModel } from '../models/accordion.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AccordionService {
+  accordions: AccordionModel[] = [
+    {
+      id: 0,
+      question: 'What is Frontend Mentor, and how will it help me?',
+      response:
+        "Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building.",
+    },
+    {
+      id: 1,
+      question: 'Is Frontend Mentor free?',
+      response:
+        'Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels.',
+    },
+    {
+      id: 2,
+      question: 'Can I use Frontend Mentor projects in my portfolio?',
+      response:
+        "Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent way to showcase your skills to potential employers!",
+    },
+    {
+      id: 3,
+      question:
+        "How can I get help if I'm stuck on a Frontend Mentor challenge?",
+      response:
+        "The best place to get help is inside Frontend Mentor's Discord community. There's a help channel where you can ask questions and seek support from other community members.",
+    },
+  ];
+
+  getAccordions() {
+    return this.accordions;
+  }
+}

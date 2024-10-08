a9c8b5e04247dbd1c1de732125dbdaca318f6382
tony1c <antoniomario.cavuto@gmail.com>
2024-10-08 09:22:07 +0200

    form builder

--- age-calculator-angular/src/app/components/age-calculator/age-calculator.component.ts
+++ age-calculator-angular/src/app/components/age-calculator/age-calculator.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-age-calculator',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './age-calculator.component.html',
   styleUrl: './age-calculator.component.css',
 })
 export class AgeCalculatorComponent {
+  #fb: FormBuilder = inject(FormBuilder);
+
+  dateOfBirth = this.#fb.group({
+    dayValue: ['', Validators.required],
+    monthValue: ['', Validators.required],
+    yearValue: ['', Validators.required],
+  });
+
+  submitForm(): void {
+    if (this.dateOfBirth.valid) {
+      const formValues = this.dateOfBirth.value;
+      console.log('Form submitted', formValues);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
   public log(): void {
     console.log('clicked');
   }

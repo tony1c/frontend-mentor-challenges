755f5eba31c7fa03836eb9956e0a43f33630980d
tony1c <antoniomario.cavuto@gmail.com>
2024-10-12 17:09:46 +0200

    removed pipe

--- age-calculator-angular/src/app/components/age-calculator/age-calculator.component.ts
+++ age-calculator-angular/src/app/components/age-calculator/age-calculator.component.ts
@@ -1,26 +1,36 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Age } from '../../models/age.model';
 
 @Component({
   selector: 'app-age-calculator',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './age-calculator.component.html',
   styleUrl: './age-calculator.component.css',
 })
 export class AgeCalculatorComponent {
   #fb: FormBuilder = inject(FormBuilder);
+  submittedValues: Age = { day: 0, month: 0, year: 0 }; // Store the submitted values here
 
   dateOfBirth = this.#fb.group({
-    dayValue: ['', Validators.required],
-    monthValue: ['', Validators.required],
-    yearValue: ['', Validators.required],
+    day: [null, Validators.required],
+    month: [null, Validators.required],
+    year: [null, Validators.required],
   });
 
   submitForm(): void {
     if (this.dateOfBirth.valid) {
-      const formValues = this.dateOfBirth.value;
+      const formValues = this.dateOfBirth.getRawValue();
       console.log('Form submitted', formValues);
+      this.dateOfBirth.patchValue({
+        day: formValues.day,
+        month: formValues.month,
+        year: formValues.year,
+      });
+      // Store the submitted values to display in the template
+      this.submittedValues = formValues as unknown as Age;
     } else {
       console.log('Form is invalid');
     }
---
+++ age-calculator-angular/src/app/models/age.model.ts
@@ -0,0 +1,5 @@
+export interface Age {
+  day: number;
+  month: number;
+  year: number;
+}

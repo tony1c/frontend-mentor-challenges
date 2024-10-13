aa51fd21eea32fb571a2fbcdff63688d5d90bda4
tony1c <antoniomario.cavuto@gmail.com>
2024-10-13 15:40:09 +0200

    footer

--- age-calculator-angular/src/app/app.component.ts
+++ age-calculator-angular/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AgeCalculatorComponent } from './components/age-calculator/age-calculator.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [AgeCalculatorComponent],
+  imports: [AgeCalculatorComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })

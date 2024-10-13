bac0ff19140d2475e73be01ef447304ae6bb3852
tony1c <antoniomario.cavuto@gmail.com>
2024-10-13 13:16:17 +0200

    save check validator v1

--- age-calculator-angular/src/app/components/age-calculator/age-calculator.component.ts
+++ age-calculator-angular/src/app/components/age-calculator/age-calculator.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { AgeCalculatorService } from '../../services/age-calculator.service';
@@ -10,16 +10,32 @@ import { Age } from '../../models/age.model';
   imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './age-calculator.component.html',
 })
-export class AgeCalculatorComponent {
+export class AgeCalculatorComponent implements OnInit {
   #fb: FormBuilder = inject(FormBuilder);
   #ageCalculatorService: AgeCalculatorService = inject(AgeCalculatorService);
   age: Age = { years: null, months: null, days: null };
+  submitted = false;
 
   dateOfBirth = this.#fb.group({
-    day: ['', Validators.required],
-    month: ['', Validators.required],
-    year: ['', Validators.required],
+    day: ['', [Validators.required, Validators.min(1), Validators.max(31)]],
+    month: ['', [Validators.required, Validators.min(1), Validators.max(12)]],
+    year: [
+      '',
+      [
+        Validators.required,
+        Validators.min(1900),
+        Validators.max(new Date().getFullYear()),
+      ],
+    ],
   });
+
+  ngOnInit() {
+    this.dateOfBirth.valueChanges.subscribe(() => {
+      if (this.submitted) {
+        this.submitted = false;
+      }
+    });
+  }
 
   public isValidDate(): boolean {
     const { day, month, year } = this.dateOfBirth.getRawValue();
@@ -39,6 +55,7 @@ export class AgeCalculatorComponent {
   }
 
   public onSubmit(): void {
+    this.submitted = true;
     if (this.isValidDate() && this.dateOfBirth.valid) {
       this.calcAge();
     } else {

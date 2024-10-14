1ec53eaeecb30d704509e2c37c4d62cb02f16b40
tony1c <antoniomario.cavuto@gmail.com>
2024-10-14 20:06:16 +0200

    first commit

---
+++ coding-bootcamp-testimonials-slider-angular/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, FooterComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'coding-bootcamp-testimonials-slider-angular';
+}

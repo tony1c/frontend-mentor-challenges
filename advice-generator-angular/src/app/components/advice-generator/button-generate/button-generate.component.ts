14d02c8b00661a6d47f06a28e6e0af206e9153e2
tony1c <antoniomario.cavuto@gmail.com>
2024-10-17 16:01:51 +0200

    fixed blur

--- advice-generator-angular/src/app/components/advice-generator/button-generate/button-generate.component.ts
+++ advice-generator-angular/src/app/components/advice-generator/button-generate/button-generate.component.ts
@@ -17,20 +17,6 @@ import {
 export class ButtonGenerateComponent {
   @Output() onGenerateAdvice = new EventEmitter<void>();
 
-  // Reset button focus on mobile after 3 seconds when clicked
-  elRef = inject(ElementRef);
-  timeoutId: ReturnType<typeof setTimeout> | null = null;
-  @HostListener('click') onClick() {
-    this.timeoutId = setTimeout(() => {
-      console.log(this.elRef);
-      this.elRef.nativeElement.blur();
-    }, 3000);
-
-    if (this.timeoutId) {
-      clearInterval(this.timeoutId);
-    }
-  }
-
   generateAdvice(): void {
     this.onGenerateAdvice.emit();
   }

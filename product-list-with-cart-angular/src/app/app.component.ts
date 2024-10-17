407d673dab17dc86ee7fa428c4c8cab98bd258de
tony1c <antoniomario.cavuto@gmail.com>
2024-10-17 21:52:05 +0200

    product component

--- product-list-with-cart-angular/src/app/app.component.ts
+++ product-list-with-cart-angular/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { Product } from './models/product.model';
 import { ProductsService } from './services/products.service';
+import { ProductComponent } from './components/product/product.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [ProductComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })

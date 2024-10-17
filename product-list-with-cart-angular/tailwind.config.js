9773473e96fac9d46e5873728538ad888ebe971c
tony1c <antoniomario.cavuto@gmail.com>
2024-10-17 20:02:22 +0200

    tailwind config

--- product-list-with-cart-angular/tailwind.config.js
+++ product-list-with-cart-angular/tailwind.config.js
@@ -5,7 +5,25 @@ module.exports = {
     fontFamily: {
       "red-hat-text": ['"Red Hat Text"', "sans-serif"],
     },
-    extend: {},
+    fontWeight: {
+      400: 400,
+      600: 600,
+      700: 700,
+    },
+    extend: {
+      colors: {
+        "c-red": "hsl(14, 86%, 42%)",
+        "c-green": "hsl(159, 69%, 38%)",
+        "c-rose": {
+          50: "hsl(20, 50%, 98%)",
+          100: "hsl(13, 31%, 94%)",
+          300: "hsl(14, 25%, 72%)",
+          400: "hsl(7, 20%, 60%)",
+          500: "hsl(12, 20%, 44%)",
+          900: "hsl(14, 65%, 9%)",
+        },
+      },
+    },
   },
   plugins: [],
 };

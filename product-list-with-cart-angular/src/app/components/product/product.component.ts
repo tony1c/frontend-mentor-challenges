cbba1a57969ed5aab2d70dc7c2e90422aa28ecc2
tony1c <antoniomario.cavuto@gmail.com>
2024-10-18 18:57:25 +0200

    product list component

--- product-list-with-cart-angular/src/app/components/product/product.component.ts
+++ product-list-with-cart-angular/src/app/components/product/product.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { AddToCartComponent } from '../add-to-cart/add-to-cart.component';
+import { Product } from '../../models/product.model';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [AddToCartComponent],
+  imports: [AddToCartComponent, CurrencyPipe],
   templateUrl: './product.component.html',
   styles: ``,
 })
-export class ProductComponent {}
+export class ProductComponent {
+  @Input({ required: true }) product!: Product;
+}

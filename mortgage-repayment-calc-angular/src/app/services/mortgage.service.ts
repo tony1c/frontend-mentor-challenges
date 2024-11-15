c7e08b4d412dc852886f16a7e9ad6e61049e3488
tony1c <antoniomario.cavuto@gmail.com>
2024-11-15 22:44:46 +0100

    checkpoint v1

--- mortgage-repayment-calc-angular/src/app/services/mortgage.service.ts
+++ mortgage-repayment-calc-angular/src/app/services/mortgage.service.ts
@@ -5,17 +5,17 @@ import { Injectable } from '@angular/core';
 })
 export class MortgageService {
   calcMortgage(
-    amount: number,
-    term: number,
-    rate: number,
+    amount: string,
+    term: string,
+    rate: string,
     type: string,
   ): number {
     if (type === 'repayment') {
       const monthsInYear = 12;
-      const monthlyInterest = rate / 100 / monthsInYear;
-      const totMonths = term * monthsInYear;
+      const monthlyInterest = Number(rate) / 100 / monthsInYear;
+      const totMonths = Number(term) * monthsInYear;
       const repayment =
-        amount *
+        Number(amount) *
         ((monthlyInterest * Math.pow(1 + monthlyInterest, totMonths)) /
           (Math.pow(1 + monthlyInterest, totMonths) - 1));
       return repayment;

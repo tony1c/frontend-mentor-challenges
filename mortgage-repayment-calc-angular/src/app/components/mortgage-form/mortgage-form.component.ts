76f2301f6c086c38d9626ce95ed17fe2d7f75811
tony1c <antoniomario.cavuto@gmail.com>
2024-11-15 19:29:48 +0100

    created and binded form inputs

--- mortgage-repayment-calc-angular/src/app/components/mortgage-form/mortgage-form.component.ts
+++ mortgage-repayment-calc-angular/src/app/components/mortgage-form/mortgage-form.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { BtnCalcComponent } from '../btn-calc/btn-calc.component';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-mortgage-form',
   standalone: true,
-  imports: [BtnCalcComponent],
+  imports: [BtnCalcComponent, ReactiveFormsModule],
   templateUrl: './mortgage-form.component.html',
   styles: ``,
 })
-export class MortgageFormComponent {}
+export class MortgageFormComponent {
+  #fb = inject(FormBuilder);
+  mortgageForm = this.#fb.group({
+    amount: ['', Validators.required],
+    term: ['', Validators.required],
+    interestRate: ['', Validators.required],
+  });
+
+  onSubmit(): void {
+    const { amount, term, interestRate } = this.mortgageForm.value;
+    console.log(
+      `Submitted with those values: ${amount}, ${term}, ${interestRate}`,
+    );
+  }
+}

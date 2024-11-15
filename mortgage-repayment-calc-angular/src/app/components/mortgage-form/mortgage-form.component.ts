1669a445a9e489bf79764022cce498db6cb9d6f7
tony1c <antoniomario.cavuto@gmail.com>
2024-11-15 19:36:09 +0100

    added type field

--- mortgage-repayment-calc-angular/src/app/components/mortgage-form/mortgage-form.component.ts
+++ mortgage-repayment-calc-angular/src/app/components/mortgage-form/mortgage-form.component.ts
@@ -15,12 +15,13 @@ export class MortgageFormComponent {
     amount: ['', Validators.required],
     term: ['', Validators.required],
     interestRate: ['', Validators.required],
+    type: ['', Validators.required],
   });
 
   onSubmit(): void {
-    const { amount, term, interestRate } = this.mortgageForm.value;
+    const { amount, term, interestRate, type } = this.mortgageForm.value;
     console.log(
-      `Submitted with those values: ${amount}, ${term}, ${interestRate}`,
+      `Submitted with those values: ${amount}, ${term}, ${interestRate}, ${type}`,
     );
   }
 }

d2b396105b76f4f25115bb502ec5b6c694bc723a
tony1c <antoniomario.cavuto@gmail.com>
2024-11-18 09:08:20 +0100

    added footer

--- mortgage-repayment-calc-angular/src/app/app.component.ts
+++ mortgage-repayment-calc-angular/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MortgageFormComponent } from './components/mortgage-form/mortgage-form.component';
 import { ResultDisplayComponent } from './components/result-display/result-display.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MortgageFormComponent, ResultDisplayComponent],
+  imports: [MortgageFormComponent, ResultDisplayComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })

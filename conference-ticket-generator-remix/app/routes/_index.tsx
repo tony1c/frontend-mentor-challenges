b903318a7bcae667c53c7383bb84dbe7b7745956
tony1c <antoniomario.cavuto@gmail.com>
2025-01-03 17:56:52 +0100

    bg decoration v1 missing desktop sizes

--- conference-ticket-generator-remix/app/routes/_index.tsx
+++ conference-ticket-generator-remix/app/routes/_index.tsx
@@ -9,55 +9,47 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <main>
-      {/* main bg */}
-      <div
-        className={
-          "relative -z-50 h-screen bg-[url('/images/background-mobile.png')] bg-cover bg-center md:bg-[url('/images/background-tablet.png')] lg:bg-[url('/images/background-desktop.png')]"
-        }
-      ></div>
-      {/* grid */}
-      <div
-        className={
-          "absolute inset-0 -z-40 bg-[url('/images/pattern-lines.svg')] bg-cover bg-center bg-no-repeat"
-        }
-      ></div>
-      {/* top line */}
-      <div className="absolute left-[263px] top-[24px] -z-30">
-        <img
-          src="/images/pattern-squiggly-line-top.svg"
-          className="h-[53px] w-[112px]"
-          alt="Line top"
-        />
-      </div>
-      {/* circle */}
-      <div className="absolute left-[313px] top-[548px] -z-30">
-        <img
-          src="/images/pattern-circle.svg"
-          className="size-[107px]"
-          alt="Circle decoration"
-        />
-      </div>
-      <div className="absolute bottom-0 -z-30">
-        <img
-          src="/images/pattern-squiggly-line-bottom.svg"
-          className="h-[210px] w-[301px]"
-          alt="Line bottom decoration"
-        />
-      </div>
-    </main>
-  );
-}
-{
-  /* <main
+    <div
       className={
-        "relative -z-50 max-h-[1100px] min-h-screen max-w-[1440px] overflow-hidden bg-[url('/images/background-mobile.png')] bg-cover bg-center md:bg-[url('/images/background-tablet.png')] lg:bg-[url('/images/background-desktop.png')]"
+        "bg-[url('/images/background-mobile.png')] bg-cover bg-center bg-no-repeat md:bg-[url('/images/background-tablet.png')] lg:bg-[url('/images/background-desktop.png')]"
       }
     >
-      <div
-        className={
-          "-z-40 h-[778px] w-[1266px] bg-[url('/images/pattern-lines.svg')] bg-center bg-no-repeat md:w-[1404px] lg:h-[1100px] lg:w-[1864px]"
-        }
-      ></div>
-    </main> */
+      <div className="relative mx-auto min-h-screen overflow-hidden">
+        {/* grid */}
+        <div
+          className={
+            "absolute h-[778px] w-[1266px] -translate-x-[446px] bg-[url('/images/pattern-lines.svg')] bg-cover bg-center md:w-[1404px] md:-translate-x-[318px] lg:left-1/2 lg:h-[1100px] lg:w-[1864px] lg:-translate-x-1/2"
+          }
+        ></div>
+        {/* top line */}
+        <div className="absolute translate-x-[263px] translate-y-[24px] md:translate-x-[536px] md:translate-y-[88px]">
+          <img
+            src="/images/pattern-squiggly-line-top.svg"
+            alt="Top line decoration"
+            className="h-[53px] w-[112px] md:h-[108px] md:w-[232px]"
+          />
+        </div>
+        {/* circle */}
+        <div className="absolute translate-x-[313px] translate-y-[548px] md:translate-x-[669px] md:translate-y-[552px]">
+          <img
+            src="/images/pattern-circle.svg"
+            alt="Top line decoration"
+            className="size-[107px] md:size-[217px]"
+          />
+        </div>
+        {/* bottom line */}
+        <div className="absolute bottom-0">
+          <img
+            src="/images/pattern-squiggly-line-bottom.svg"
+            alt="Bottom line decoration"
+            className="h-[210px] w-[301px] md:h-[276px] md:w-[396px]"
+          />
+        </div>
+        {/* content */}
+        <main className="container relative mx-auto text-c-neutral-0">
+          <h1 className="text-preset-1">Hello</h1>
+        </main>
+      </div>
+    </div>
+  );
 }

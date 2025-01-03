e56dd85c2b2fa06d5a1c04a33b024671f5f827a5
tony1c <antoniomario.cavuto@gmail.com>
2025-01-03 11:52:01 +0100

    colors setup

--- conference-ticket-generator-remix/tailwind.config.ts
+++ conference-ticket-generator-remix/tailwind.config.ts
@@ -5,9 +5,20 @@ export default {
   theme: {
     extend: {
       colors: {
-        neutral: {
+        'c-neutral': {
           900: '#0D082D',
+          700: '#4B4869',
+          500: '#8784A5',
+          300: '#D1D0D5',
+          0: '#fff',
         },
+        'c-orange': {
+          700: '#E1604F',
+          500: '#F57463',
+        },
+      },
+      backgroundImage: {
+        'c-gradient-01': 'linear-gradient(90deg, #F37362 0%, #FFF 100%);',
       },
       fontFamily: {
         sans: [

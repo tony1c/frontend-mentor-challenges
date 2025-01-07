fb55b77457098e3bfcdcb748d53a0e0413a87a52
tony1c <antoniomario.cavuto@gmail.com>
2025-01-07 16:07:15 +0100

    netlify edge function

--- conference-ticket-generator-remix/vite.config.ts
+++ conference-ticket-generator-remix/vite.config.ts
@@ -1,7 +1,7 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
+import { netlifyPlugin } from "@netlify/remix-edge-adapter/plugin";
 
 declare module "@remix-run/node" {
   interface Future {

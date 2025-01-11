ca955d6a4148631b26a83df432a998e4b817768f
tony1c <antoniomario.cavuto@gmail.com>
2025-01-11 12:47:37 +0100

    added astro netlify

--- social-proof-section/astro.config.mjs
+++ social-proof-section/astro.config.mjs
@@ -2,8 +2,11 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify/static";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  output: "server",
+  adapter: netlify(),
 });

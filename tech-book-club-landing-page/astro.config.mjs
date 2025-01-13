f5530da1e0c4aba5bd1adc72289134f8bde3477c
tony1c <antoniomario.cavuto@gmail.com>
2025-01-13 01:31:54 +0100

    prettier setup done

--- tech-book-club-landing-page/astro.config.mjs
+++ tech-book-club-landing-page/astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import netlify from '@astrojs/netlify';
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  adapter: netlify()
+  adapter: netlify(),
 });

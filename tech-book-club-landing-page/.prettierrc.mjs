44319808b48ff19fab83c9a38ed7e7b6e385a15a
tony1c <antoniomario.cavuto@gmail.com>
2025-01-13 02:23:42 +0100

    prettierrc

---
+++ tech-book-club-landing-page/.prettierrc.mjs
@@ -0,0 +1,19 @@
+// .prettierrc.mjs
+/** @type {import("prettier").Config} */
+export default {
+  singleQuote: true,
+  jsxSingleQuote: true,
+  bracketSameLine: true,
+  tabWidth: 2,
+  arrowParens: 'avoid',
+  astroAllowShorthand: true,
+  plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+};

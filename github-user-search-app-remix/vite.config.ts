9bcac34b6f6be69b9e1dc7a7396f7682818ba9b4
tony1c <antoniomario.cavuto@gmail.com>
2025-01-21 08:53:17 +0100

    github user search app first commit

---
+++ github-user-search-app-remix/vite.config.ts
@@ -0,0 +1,27 @@
+import { vitePlugin as remix } from '@remix-run/dev';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { netlifyPlugin } from '@netlify/remix-adapter/plugin';
+
+declare module '@remix-run/node' {
+  // or cloudflare, deno, etc.
+  interface Future {
+    v3_singleFetch: true;
+  }
+}
+
+export default defineConfig({
+  plugins: [
+    remix({
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_lazyRouteDiscovery: true,
+        v3_throwAbortReason: true,
+        v3_singleFetch: true,
+      },
+    }),
+    netlifyPlugin(),
+    tsconfigPaths(),
+  ],
+});

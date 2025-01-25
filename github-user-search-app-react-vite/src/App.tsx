5ff19d0b038fb80091674aa7b6f6d1dc79bf1f14
tony1c <antoniomario.cavuto@gmail.com>
2025-01-25 17:42:35 +0100

    github-user-search-app in react-vite

---
+++ github-user-search-app-react-vite/src/App.tsx
@@ -0,0 +1,11 @@
+import { ThemeToggle } from './components/ThemeToggle';
+
+function App() {
+  return (
+    <main className='bg-c-light-F6F8FF dark:bg-c-dark-141D2F h-screen'>
+      <ThemeToggle />
+    </main>
+  );
+}
+
+export default App;

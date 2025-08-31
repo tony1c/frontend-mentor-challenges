bdb87d2e82c13f1f0115f6fdbe869b780ff149e6
Tony <antoniomario.cavuto@gmail.com>
2025-08-31 22:36:41 +0200

    Input validators working

--- url-shortening-api-angular/src/app/components/input/input.ts
+++ url-shortening-api-angular/src/app/components/input/input.ts
@@ -1,4 +1,5 @@
-import { Component, inject } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
 import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 import { ShortenService } from '../../services/shorten';
@@ -6,22 +7,41 @@ import { urlValidator } from '../../validators/url-validator';
 
 @Component({
   selector: 'app-input',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgClass],
   templateUrl: './input.html',
   styles: ``,
 })
 export class Input {
   shortenService = inject(ShortenService);
+  isSubmitted = signal(false);
   url = new FormControl<string>('', {
     nonNullable: true,
     validators: [Validators.required, urlValidator()],
   });
 
   onSubmit(): void {
-    if (this.url.invalid) return;
+    this.isSubmitted.set(true);
+
+    if (this.url.invalid) {
+      return;
+    }
 
     this.shortenService.shortenAndAdd(this.url.value).subscribe({
       next: () => this.url.reset(),
+      error: () => {
+        this.url.setErrors({ invalidUrl: true });
+      },
     });
+  }
+
+  public onInputChange(): void {
+    if (this.url.invalid && this.isSubmitted()) {
+      this.url.setErrors(null);
+      this.isSubmitted.set(false);
+    }
+  }
+
+  isInvalid(): boolean {
+    return this.url.invalid && this.isSubmitted() && (this.url.touched || this.url.dirty);
   }
 }

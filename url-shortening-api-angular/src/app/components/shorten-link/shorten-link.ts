bb6d0c38b470ffe1064dc61c7846074556413ebc
Tony <antoniomario.cavuto@gmail.com>
2025-08-31 12:18:32 +0200

    shorten mobile

--- url-shortening-api-angular/src/app/components/shorten-link/shorten-link.ts
+++ url-shortening-api-angular/src/app/components/shorten-link/shorten-link.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
 
 import { ShortenedUrl } from '../../models/shorten';
 
@@ -10,4 +10,14 @@ import { ShortenedUrl } from '../../models/shorten';
 })
 export class ShortenLink {
   shortenUrl = input.required<ShortenedUrl>();
+  buttonText = signal('Copy');
+
+  onClick(): void {
+    navigator.clipboard.writeText(this.shortenUrl().shortenedUrl);
+    this.buttonText.set('Copied!');
+
+    setTimeout(() => {
+      this.buttonText.set('Copy');
+    }, 3000);
+  }
 }

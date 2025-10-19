a18b4a1a4db8e3659324a465631e33e5c267d28f
Tony <antoniomario.cavuto@gmail.com>
2025-10-19 22:35:50 +0200

    final clean layout

--- interactive-card-details-form-ng/src/app/app.ts
+++ interactive-card-details-form-ng/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 
 import { BackCard } from './components/cards/back-card/back-card';
 import { FrontCard } from './components/cards/front-card/front-card';
+import { Form } from './components/form/form';
 
 @Component({
   selector: 'app-root',
-  imports: [BackCard, FrontCard],
+  imports: [BackCard, FrontCard, Form],
   templateUrl: './app.html',
   styles: ``,
 })
